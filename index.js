0e36a196f6b4d0714cdf13fc72ee74a94cda60eb
unknown <jeleznov88@gmail.com>
2019-03-03 12:30:04 +0300

    random box position

--- index.js
+++ index.js
@@ -25,14 +25,16 @@ function handleBoxClick(event) {
 function renderBox() {
   $game.innerHTML = ''
   var box = document.createElement('div')
-  var boxSize = gerRandom(30, 100)
-
+  var boxSize = getRandom(30, 100)
+  var gameSize = $game.getBoundingClientRect()
+  var maxTop = gameSize.height - boxSize
+  var maxLeft = gameSize.width - boxSize
 
   box.style.height = box.style.width = boxSize + 'px'
   box.style.position = 'absolute'
   box.style.backgroundColor = '#000'
-  box.style.top = '50px'
-  box.style.left = '70px'
+  box.style.top = getRandom(0, maxTop) + 'px'
+  box.style.left = getRandom(0, maxLeft) + 'px'
   box.style.cursor = 'pointer'
   box.setAttribute('data-box', 'true')
 
@@ -41,6 +43,6 @@ function renderBox() {
 
 }
 
-function gerRandom(min, max) {
+function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min) + min)
 }
