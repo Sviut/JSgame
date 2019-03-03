afea9ce8673e43826b78a8a849cb6a5c3b06ddd9
unknown <jeleznov88@gmail.com>
2019-03-03 12:24:14 +0300

    random size box

---
+++ index.js
@@ -0,0 +1,46 @@
+var $start = document.querySelector('#start')
+var $game = document.querySelector('#game')
+
+var score = 0
+
+
+$start.addEventListener('click', startGame)
+$game.addEventListener('click', handleBoxClick)
+
+function startGame() {
+  $game.style.backgroundColor = '#fff'
+  $start.classList.add('hide')
+
+  renderBox()
+}
+
+function handleBoxClick(event) {
+  if (event.target.dataset.box) {
+    score++
+    renderBox()
+  }
+}
+
+
+function renderBox() {
+  $game.innerHTML = ''
+  var box = document.createElement('div')
+  var boxSize = gerRandom(30, 100)
+
+
+  box.style.height = box.style.width = boxSize + 'px'
+  box.style.position = 'absolute'
+  box.style.backgroundColor = '#000'
+  box.style.top = '50px'
+  box.style.left = '70px'
+  box.style.cursor = 'pointer'
+  box.setAttribute('data-box', 'true')
+
+
+  $game.insertAdjacentElement('afterbegin', box)
+
+}
+
+function gerRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
